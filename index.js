63d77709bb1269b37e09ae028a4320445a51d06c
Ed Meyer <edmeyer8851@gmail.com>
2022-11-21 23:41:51 -0500

    commit tree af0194fd440c4d9d4f58fc354bd9806f9e613282

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = drivers => {
+    const firstTwoDrivers = drivers.slice(0, 2);
+    return firstTwoDrivers;
+}
+
+const returnLastTwoDrivers = drivers => {
+    const lastTwoDrivers = drivers.slice(drivers.length-2, drivers.length);
+    return lastTwoDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (number){
+    return fare => fare * number;
+}
+
+function fareDoubler (fare){
+    const getMultiplier = createFareMultiplier;
+    return getMultiplier(2)(fare);
+}
+
+function fareTripler (fare){
+    const getMultiplier = createFareMultiplier;
+    return getMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(names, fn){
+    return fn(names);
+}
